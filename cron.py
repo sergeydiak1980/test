c7552a31fa936dd7aed6f2e4a748ac51cb8325ff
Jora <61252756+sergeydiak1980@users.noreply.github.com>
2023-05-26 13:54:04 +0300

    Update cron.py

--- cron.py
+++ cron.py
@@ -5,7 +5,7 @@ import subprocess
 
 def run_program():
     # Здесь поместите ваш код программы
-    subprocess.call(["python", "CVE.py"])  #путь к  скрипту
+    subprocess.call(["python", "main.py"])  #путь к  скрипту
 
 # Запуск программы ежедневно в удобное для вас время (в данном случае - в 9:00 утра)
 schedule.every().day.at("09:00").do(run_program)
